6b1193a7c297797bb0c081e33d4956618c4c30e2
Kaszanas <34846245+Kaszanas@users.noreply.github.com>
2025-05-18 02:27:21 +0200

    refactor(settings.py): moved SUFFIX to global settings

--- src/sc2_combat_detector/settings.py
+++ src/sc2_combat_detector/settings.py
@@ -1,1 +1,4 @@
 LOGGING_FORMAT = "[%(asctime)s][%(process)d/%(thread)d][%(levelname)s][%(filename)s:%(lineno)s] - %(message)s"
+
+# Suffix for cache files, this is used in multiple places:
+SUFFIX = ".binpb"
