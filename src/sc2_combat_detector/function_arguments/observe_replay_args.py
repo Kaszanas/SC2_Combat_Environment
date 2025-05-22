9bfea77a4d1e03dee375adfee2ac31249316dc4d
Kaszanas <34846245+Kaszanas@users.noreply.github.com>
2025-05-22 21:59:16 +0200

    refactor(observe_replay_args.py): adjusted defaults, added no_skips argument

--- src/sc2_combat_detector/function_arguments/observe_replay_args.py
+++ src/sc2_combat_detector/function_arguments/observe_replay_args.py
@@ -8,13 +8,14 @@ from typing import List
 @dataclass
 class ObserveReplayArgs:
     replay_path: Path
+    no_skips: bool
+    gameloops_to_observe: List[int]
     render: bool = True
     feature_screen_size: int | None = None  # 84,
     feature_minimap_size: int | None = None  # 64,
     feature_camera_width: int = 24
-    rgb_screen_size: str = "128,96"
+    rgb_screen_size: str = "640,480"
     rgb_minimap_size: str = "16"
-    gameloops_to_observe: List[int] = []
 
     @staticmethod
     def get_initial_processing_args(replay_path: Path) -> ObserveReplayArgs:
@@ -24,21 +25,25 @@ class ObserveReplayArgs:
             feature_screen_size=None,
             feature_minimap_size=None,
             feature_camera_width=24,
-            rgb_screen_size="128,96",
-            rgb_minimap_size="16",
+            rgb_screen_size="640,480",
+            rgb_minimap_size="128",
+            no_skips=False,
+            gameloops_to_observe=[],
         )
 
     @staticmethod
     def get_combat_processing_args(
-        replay_path: Path, gameloops_to_observe: List[int]
+        replay_path: Path,
+        gameloops_to_observe: List[int],
     ) -> ObserveReplayArgs:
         return ObserveReplayArgs(
             replay_path=replay_path,
-            render=True,
+            render=False,
             feature_screen_size=None,
             feature_minimap_size=None,
             feature_camera_width=24,
-            rgb_screen_size="128,96",
-            rgb_minimap_size="16",
+            rgb_screen_size="640,480",
+            rgb_minimap_size="128",
+            no_skips=True,
             gameloops_to_observe=gameloops_to_observe,
         )
