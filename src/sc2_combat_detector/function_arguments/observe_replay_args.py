b0642ff1ba9632a50ee31f19deb62245aac9afc5
Kaszanas <34846245+Kaszanas@users.noreply.github.com>
2025-05-23 00:20:02 +0200

    feat(observe_replay_args.py): combats_to_observe is now optional
    BREAKING CHANGE:

--- src/sc2_combat_detector/function_arguments/observe_replay_args.py
+++ src/sc2_combat_detector/function_arguments/observe_replay_args.py
@@ -4,12 +4,15 @@ from dataclasses import dataclass
 from pathlib import Path
 from typing import List
 
+from sc2_combat_detector.detector.detect_combat import FileDetectCombatResult
+from sc2_combat_detector.proto import observation_collection_pb2 as obs_collection_pb
+
 
 @dataclass
 class ObserveReplayArgs:
     replay_path: Path
     no_skips: bool
-    gameloops_to_observe: List[int]
+    combats_to_observe: FileDetectCombatResult | None
     render: bool = True
     feature_screen_size: int | None = None  # 84,
     feature_minimap_size: int | None = None  # 64,
@@ -28,13 +31,13 @@ class ObserveReplayArgs:
             rgb_screen_size="640,480",
             rgb_minimap_size="128",
             no_skips=False,
-            gameloops_to_observe=[],
+            combats_to_observe=None,
         )
 
     @staticmethod
     def get_combat_processing_args(
         replay_path: Path,
-        gameloops_to_observe: List[int],
+        combats_to_observe: List[obs_collection_pb.ObservationInterval],
     ) -> ObserveReplayArgs:
         return ObserveReplayArgs(
             replay_path=replay_path,
@@ -45,5 +48,5 @@ class ObserveReplayArgs:
             rgb_screen_size="640,480",
             rgb_minimap_size="128",
             no_skips=True,
-            gameloops_to_observe=gameloops_to_observe,
+            combats_to_observe=combats_to_observe,
         )
