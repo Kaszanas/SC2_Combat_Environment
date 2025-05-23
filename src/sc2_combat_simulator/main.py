61d0c04eecb0b7f7c95017c5e63832466bfcd84b
Kaszanas <34846245+Kaszanas@users.noreply.github.com>
2025-05-24 00:40:18 +0200

    feat(combat_simulator): drafting, added comments

--- src/sc2_combat_simulator/main.py
+++ src/sc2_combat_simulator/main.py
@@ -1,5 +1,52 @@
-def main():
-    pass
+import enum
+import logging
+from pathlib import Path
+import click
+
+from sc2_combat_simulator import sc2_combat_simulator
+from sc2_combat_simulator.settings import LOGGING_FORMAT
+
+
+class LogLevel(str, enum.Enum):
+    """Log levels for the application."""
+
+    DEBUG = "DEBUG"
+    INFO = "INFO"
+    WARNING = "WARNING"
+    ERROR = "ERROR"
+    CRITICAL = "CRITICAL"
+
+
+@click.command(
+    help="Runs combat simulations with agents. Leverages the output from sc2_combat_detector."
+)
+@click.option(
+    "--combat_detection_dir",
+    type=click.Path(
+        dir_okay=True,
+        file_okay=False,
+        resolve_path=True,
+        path_type=Path,
+    ),
+    required=True,
+    help="",
+)
+@click.option(
+    "--log",
+    type=click.Choice(list(LogLevel), case_sensitive=False),
+    default=LogLevel.WARNING,
+    help="Log level. Default is WARNING.",
+)
+def main(
+    combat_detection_dir: Path,
+    log: LogLevel,
+):
+    numeric_level = getattr(logging, log.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError(f"Invalid log level: {numeric_level}")
+    logging.basicConfig(level=logging.INFO, format=LOGGING_FORMAT)
+
+    sc2_combat_simulator(combat_detection_dir=combat_detection_dir)
 
 
 if __name__ == "__main__":
