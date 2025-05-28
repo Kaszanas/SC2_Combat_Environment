1b8694d2c1fa7da1c11c175f91e6a09d28e96655
Kaszanas <34846245+Kaszanas@users.noreply.github.com>
2025-05-28 18:03:29 +0200

    feat(settings.pty): added MAPS set for registering maps
    This may be unused later because it seems that the pysc2 prefers saving the registered maps to the global variables.

--- src/sc2_combat_simulator/settings.py
+++ src/sc2_combat_simulator/settings.py
@@ -1,4 +1,7 @@
 from pathlib import Path
+from typing import Set
+
+from pysc2_evolved.maps.lib import Map
 
 
 LOGGING_FORMAT = "[%(asctime)s][%(process)d/%(thread)d][%(levelname)s][%(filename)s:%(lineno)s] - %(message)s"
@@ -6,3 +9,9 @@ LOGGING_FORMAT = "[%(asctime)s][%(process)d/%(thread)d][%(levelname)s][%(filenam
 REPLAY_DIR = Path("./data/agent_replays").resolve()
 if not REPLAY_DIR.exists():
     REPLAY_DIR.mkdir(parents=True, exist_ok=True)
+
+# REVIEW: Is there a better way of doing this?
+# REVIEW: Unfortunately pysc2_evolved relies on finding registered subclasses of Map to work.
+# This will be filled dynamically when running the combat_simulator.
+# Map class instances are created as needed when loading the combat detection data.
+MAPS: Set[Map] = {}
